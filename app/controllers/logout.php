5faa5c7e0cd46c33fc525258a6a4d83543b0efb1
KatarzynaPiazdecka <katarzynaPiazdecka@gmail.com>
2015-03-04 21:56:13 +0100

    Update logout.php

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../app/init.php';
 
-$user = new User();
-$user->logout();
+$user = new User(); // we create a new user object to make use of the functionality inside of our user class
+$user->logout(); // we logout
 
 Redirect::to('index.php');
