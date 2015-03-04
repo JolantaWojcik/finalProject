75c39b1ae346b486e4ac1725c837cfe2b8f8fde6
JolantaWojcik <jola.j.wk@gmail.com>
2015-03-04 23:07:33 +0100

    Update login.php

--- public/templates/login.php
+++ public/templates/login.php
@@ -1,5 +1,5 @@
-﻿<form action="" method="post">
-	<div class="field">
+﻿<form action="" method="post"> //we outline the form that we're going to create, which is going to allow them to enter their credentials they need to sign in. Action is blank so we submit back to this page.
+	<div class="field"> //now we crate similar markup as in Registration
 		<label for="username">Login</label>
 		<input type="text" name="login" id="login" autocomplete="off">
 	</div>
@@ -10,7 +10,7 @@
 	</div>
 	
 	<div class="field">
-		<label for="remember">
+		<label for="remember"> //text is clickable because we take input in the lable
 			<input type="checkbox" name="remember" id="remember"> Remember me
 		</label>
 	</div>
