f9586ccd0d1171cb5eddbe0ad2d38dd906d23f7b
JolantaWojcik <jola.j.wk@gmail.com>
2015-03-04 22:42:42 +0100

    Update Token.php

--- app/models/Token.php
+++ app/models/Token.php
@@ -1,14 +1,18 @@
 <?php
 class Token {
-	public static function generate() {  //token, md5(uniqid)
-		return Session::put(Config::get('session/token_name'), md5(uniqid()));
+	public static function generate() {  //token, md5(uniqid)// we create a static method to generate a token for us
+		return Session::put(Config::get('session/token_name'), md5(uniqid())); //we have to build Session class!!!
 	}
-	
-	public static function check($token) {
+	// now we need the ability to check if the token exists or not
+	// so we also need an ability to get a token from our session and check if it's the same as the token defined in the form
+	public static function check($token) { // we want to check if the token exists in this session
 		$tokenName = Config::get('session/token_name');
 		
+		// If the token equals the session that's currently being applied /if the token is being applied in the session/,
+		//then we want to delete that session, return true if that's the case.
+		
 		if(Session::exists($tokenName) && $token === Session::get($tokenName)) {
-			Session::delete($tokenName);
+			Session::delete($tokenName); // we delete the session by token name
 			return true;
 		}
 		
